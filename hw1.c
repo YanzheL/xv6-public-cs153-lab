53cd52e974ea24c5899b614078253ec87abd79e2
YanzheL <lee.yanzhe@yanzhe.org>
2018-10-12 15:38:47 +0800

    add more dbg info

--- hw1.c
+++ hw1.c
@@ -5,23 +5,19 @@ int main(void){
     int pid;
     printf(1,"Main(%d)\n",getpid());
     if(!(pid=fork())){
-        printf(1,"\tA[pid=%d]\t[getpid=%d]\n",pid,getpid());
+//        printf(1,"\tA[pid=%d]\t[getpid=%d]\n",pid,getpid());
         while((count<2)&&(pid=fork())){
             count++;
-            // waitpid(pid,0,0);
-            printf(1,"%d",count);
-            printf(1,"\tB[pid=%d]\t[getpid=%d]\n",pid,getpid());
+	    printf(1,"[A,ct=%d,pid=%d,getpid=%d]\n",count,pid,getpid());
         }
         if(count>0){
-            printf(1,"%d",count);
-            printf(1,"\tC[pid=%d]\t[getpid=%d]\n",pid,getpid());
+	    printf(1,"[B,ct=%d,pid=%d,getpid=%d]\n",count,pid,getpid());
         }
     }
     if(pid){
         waitpid(pid,0,0);
         count=count<<1;
-        printf(1,"%d",count);
-        printf(1,"\tD[pid=%d]\t[getpid=%d]\n",pid,getpid());
+	printf(1,"[C,ct=%d,pid=%d,getpid=%d]\n",count,pid,getpid());
     }
     exit(0);
 }
