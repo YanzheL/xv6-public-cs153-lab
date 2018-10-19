457cccf4bfc59ea51477d2878e7fc92c27ad7930
YanzheL <lee.yanzhe@yanzhe.org>
2018-10-19 09:40:36 -0700

    add priority scheduler

--- proc.c
+++ proc.c
@@ -319,37 +319,66 @@ wait(void)
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
+struct proc *
+candidate(struct spinlock *lk)
+{
+  if (lk == 0)
+    panic("schedule without lock");
+  if (lk != &ptable.lock) {
+    acquire(&ptable.lock);
+    release(lk);
+  }
+
+  struct proc *p;
+  struct proc *cd = 0;
+  int max = 0;
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; ++p) {
+    if (p->state != RUNNABLE)
+      continue;
+    if (p->priority > max) {
+      max = p->priority;
+      cd = p;
+    }
+  }
+
+  // Reacquire original lock.
+  if (lk != &ptable.lock) {  //DOC: sleeplock2
+    release(&ptable.lock);
+    acquire(lk);
+  }
+  if (cd == 0)
+    panic("No runnable process");
+  return cd;
+}
+
 void
 scheduler(void)
 {
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
-  
+
   for(;;){
     // Enable interrupts on this processor.
     sti();
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+    p = candidate(&ptable.lock);
+    // Switch to chosen process.  It is the process's job
+    // to release ptable.lock and then reacquire it
+    // before jumping back to us.
+    c->proc = p;
+    switchuvm(p);
+    p->state = RUNNING;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+    swtch(&(c->scheduler), p->context);
+    switchkvm();
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+    // Process is done running for now.
+    // It should have changed its p->state before coming back.
+    c->proc = 0;
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
     release(&ptable.lock);
 
   }
