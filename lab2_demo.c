8bef218507593fa8df69fe2d3b5e04357b6a97a4
YanzheL <lee.yanzhe@yanzhe.org>
2018-10-19 17:18:25 -0700

    add lab2_demo

--- lab2_demo.c
+++ lab2_demo.c
@@ -2,3 +2,31 @@
 // Created by Trinity on 19/10/2018.
 //
 
+#include "types.h"
+#include "user.h"
+
+#define N 5
+
+int main(void)
+{
+  chg_priority(50);
+  procdump();
+  int i;
+  for (i = 0; i < N; ++i) {
+    int pid = fork();
+    if(pid!=0){
+
+    }
+    else{
+      chg_priority(100);
+      sleep(500);
+      exit();
+    }
+  }
+  printf(1,"------------------------------\n");
+  procdump();
+  while (wait()!=-1);
+  printf(1,"------------------------------\n");
+  procdump();
+  exit();
+}
