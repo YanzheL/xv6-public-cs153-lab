c15ee2e2b87130bc2442f76f8111807f1ce5560f
YanzheL <lee.yanzhe@yanzhe.org>
2018-10-22 00:13:31 -0700

    add test workload

--- lab2_demo.c
+++ lab2_demo.c
@@ -6,11 +6,31 @@
 #include "user.h"
 #include "param.h"
 
-#define N 256
+#define N 16
+
+int isprime(int n){
+  int i;
+  for (i = 2; i < n; ++i) {
+    if(n%2==0)
+      return 0;
+  }
+  return 1;
+}
+
+void worker(int p)
+{
+  chg_priority(p);
+  int i;
+  for (i = 0; i < 10000; ++i) {
+    isprime(i);
+  }
+  sleep(200);
+  exit();
+}
 
 int main(void)
 {
-  chg_priority(30);
+  chg_priority(15);
   procdump();
   int i;
   for (i = 0; i < N; ++i) {
@@ -19,15 +39,17 @@ int main(void)
 
     }
     else{
-//      chg_priority(100);
-      sleep(200);
-      exit();
+      worker(i);
     }
   }
-  printf(1,"------------------------------\n");
+  printf(1,"--------------------       after fork all        --------------------\n");
   procdump();
+  sleep(300);
+  printf(1,"-------------------- now all of child are zombie --------------------\n");
+  procdump();
+  printf(1,"--------------------   begin waiting for child   --------------------\n");
   while (wait()!=-1);
-  printf(1,"------------------------------\n");
+  printf(1,"--------------------     cleaned all child       --------------------\n");
   procdump();
   exit();
 }
