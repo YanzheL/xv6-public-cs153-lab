07e9f66689b14696bd72575df9c10ecd203ef9b7
YanzheL <lee.yanzhe@yanzhe.org>
2018-11-18 02:45:11 -0800

    update addr check method to fit new mem layout

--- syscall.c
+++ syscall.c
@@ -19,7 +19,7 @@ fetchint(uint addr, int *ip)
 {
   struct proc *curproc = myproc();
 
-  if(addr >= curproc->sz || addr+4 > curproc->sz)
+  if ((addr >= curproc->sz && addr < KERNBASE - PGSIZE - curproc->ssz) || addr+4 >= KERNBASE - PGSIZE)
     return -1;
   *ip = *(int*)(addr);
   return 0;
@@ -34,10 +34,13 @@ fetchstr(uint addr, char **pp)
   char *s, *ep;
   struct proc *curproc = myproc();
 
-  if(addr >= curproc->sz)
+  if ((addr >= curproc->sz && addr < KERNBASE - PGSIZE - curproc->ssz) || addr >= KERNBASE - PGSIZE)
     return -1;
   *pp = (char*)addr;
-  ep = (char*)curproc->sz;
+  if(addr < curproc->sz)
+    ep = (char *) curproc->sz;
+  else
+    ep = (char *) KERNBASE - PGSIZE;
   for(s = *pp; s < ep; s++){
     if(*s == 0)
       return s - *pp;
@@ -63,7 +66,7 @@ argptr(int n, char **pp, int size)
  
   if(argint(n, &i) < 0)
     return -1;
-  if(size < 0 || (uint)i >= curproc->sz || (uint)i+size > curproc->sz)
+  if (size < 0 || ((uint)i >= curproc->sz && (uint)i < KERNBASE - PGSIZE - curproc->ssz) || (uint)i+size > KERNBASE - PGSIZE)
     return -1;
   *pp = (char*)i;
   return 0;
