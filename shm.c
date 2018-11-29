7c1a32000709e5aca44a94ef4e15fe82814c0bc4
YanzheL <lee.yanzhe@yanzhe.org>
2018-11-29 14:44:29 -0800

    finished, beta v1

--- shm.c
+++ shm.c
@@ -33,21 +33,79 @@ void shminit()
 
 int shm_open(int id, char **pointer)
 {
+  acquire(&shm_table.lock);
+  struct shm_page *p;
+  struct shm_page *np = 0;
+  struct shm_page *end = shm_table.shm_pages + 64;
+  struct proc *curproc = myproc();
+  char *mem;
 
-//you write this
+  for (p = shm_table.shm_pages; p < end; ++p) {
+    if(p->id == 0)
+      np = p; // Find available slot in same loop
+    if(p->id == id)
+      break;
+  }
+  if(p == end) { // Not found
+    if(np == 0)
+      goto bad;
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memset(mem, 0, PGSIZE);
+    if(mappages(curproc->pgdir, (void *) curproc->shmtop, PGSIZE, V2P(mem), PTE_U | PTE_W) < 0) {
+      kfree(mem);
+      goto bad;
+    }
+    pgref_inc(V2P(mem));
+    *pointer = (char *) curproc->shmtop;
+    np->id = id;
+    np->frame = mem;
+    np->refcnt = 1;
+    curproc->shmtop += PGSIZE;
+  } else { // Found
+    mem = p->frame;
+    if(mappages(curproc->pgdir, (void *) curproc->shmtop, PGSIZE, V2P(mem), PTE_U | PTE_W) < 0)
+      goto bad;
+    pgref_inc(V2P(mem));
+    *pointer = (char *) curproc->shmtop;
+    ++p->refcnt;
+    curproc->shmtop += PGSIZE;
+  }
 
+  lcr3(V2P(curproc->pgdir));
+  release(&shm_table.lock);
+  return 0;
 
-
-
-  return 0; //added to remove compiler warning -- you should decide what to return
+  bad:
+  lcr3(V2P(curproc->pgdir));
+  release(&shm_table.lock);
+  return -1;
 }
 
 int shm_close(int id)
 {
-//you write this too!
+  acquire(&shm_table.lock);
+  struct shm_page *p;
+  struct shm_page *end = shm_table.shm_pages + 64;
 
+  for (p = shm_table.shm_pages; p < end; ++p) {
+    if(p->id == id)
+      break;
+  }
+  if(p == end) // Not found
+    goto bad;
 
+  if(p->refcnt <= 1) {
+    p->id = 0;
+    kfree(p->frame);
+    p->frame = 0;
+  } else
+    --p->refcnt;
 
+  release(&shm_table.lock);
+  return 0;
 
-  return 0; //added to remove compiler warning -- you should decide what to return
+  bad:
+  release(&shm_table.lock);
+  return -1;
 }
