504d491c8b71cb88ac023b53317ae6ba2771c686
YanzheL <lee.yanzhe@yanzhe.org>
2018-11-29 14:28:19 -0800

    add field shmtop in struct proc

--- proc.h
+++ proc.h
@@ -38,7 +38,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 struct proc {
   uint sz;                     // Size of process memory (bytes), equal to text + code + heap, not aligned
   uint ssz;                    // Size of process stack (bytes), always aligned to PGSIZE
-  uint shmtop                  // Top of shared memory region
+  uint shmtop;                 // Top of shared memory region
   uint hbtm;                   // Bottom of process heap (bytes)
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
