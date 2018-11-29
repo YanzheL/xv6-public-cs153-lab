07f447c583e4374749be756ea588c72a903338c1
YanzheL <lee.yanzhe@yanzhe.org>
2018-11-29 14:26:23 -0800

    reformat

--- shm.c
+++ shm.c
@@ -7,43 +7,47 @@
 #include "proc.h"
 #include "spinlock.h"
 
-struct {
+struct
+{
   struct spinlock lock;
-  struct shm_page {
+  struct shm_page
+  {
     uint id;
     char *frame;
     int refcnt;
   } shm_pages[64];
 } shm_table;
 
-void shminit() {
+void shminit()
+{
   int i;
   initlock(&(shm_table.lock), "SHM lock");
   acquire(&(shm_table.lock));
-  for (i = 0; i< 64; i++) {
-    shm_table.shm_pages[i].id =0;
-    shm_table.shm_pages[i].frame =0;
-    shm_table.shm_pages[i].refcnt =0;
+  for (i = 0; i < 64; i++) {
+    shm_table.shm_pages[i].id = 0;
+    shm_table.shm_pages[i].frame = 0;
+    shm_table.shm_pages[i].refcnt = 0;
   }
   release(&(shm_table.lock));
 }
 
-int shm_open(int id, char **pointer) {
+int shm_open(int id, char **pointer)
+{
 
 //you write this
 
 
 
 
-return 0; //added to remove compiler warning -- you should decide what to return
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
 
-
-int shm_close(int id) {
+int shm_close(int id)
+{
 //you write this too!
 
 
 
 
-return 0; //added to remove compiler warning -- you should decide what to return
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
